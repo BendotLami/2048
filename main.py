cd0ab6ea1e2d5fe48ae95f34c6c9c321908c3e17
Benjamin <Bendotlami@gmail.com>
2021-12-12 21:29:00 +0200

    Added merge and while loop for game

--- main.py
+++ main.py
@@ -60,20 +60,63 @@ class Game:
     def get_empty_spots(self) -> np.ndarray:
         return self._grid.get_empty_spot()
 
-    def fill_initial(self):
+    def _fill_random_cells(self, size):
         spots = self.get_empty_spots()
         np.random.shuffle(spots)
-        empty_spots = spots[0: self._initial_number_of_cells]
-        random_values = np.random.choice([2, 4], p=[0.75, 0.25], size=2)
+        empty_spots = spots[0: size]
+        random_values = np.random.choice([2, 4], p=[0.75, 0.25], size=size)
         self._grid.array[empty_spots[:, 0], empty_spots[:, 1]] = random_values
+
+    def fill_initial(self):
+        self._fill_random_cells(self._initial_number_of_cells)
+
+    def fill_random_cell(self):
+        self._fill_random_cells(1)
+
+
+    def _merge_relevant(self, direction):
+        """
+        To be called after move!
+        :param direction: Move direction
+        :return: None
+        """
+        def _merge_array(array):
+            """
+            default is right move
+            """
+            i = len(array) - 1  # last element
+            while i > 0:  # iterate backward
+                if array[i] == array[i - 1]:  # same number, merge
+                    array[i] *= 2
+                    array[i - 1] = 0
+                    i -= 1
+                i -= 1
+            return array
+
+        for i in range(self._grid.grid_size):
+            if direction == 'r':
+                curr_row = self._grid.array[i, :]
+                self._grid.array[i, :] = _merge_array(curr_row[::-1])[::-1]
+            elif direction == 'l':
+                curr_row = self._grid.array[i, :]
+                self._grid.array[i, :] = _merge_array(curr_row)
+            elif direction == 'd':
+                curr_row = self._grid.array[:, i]
+                self._grid.array[:, i] = _merge_array(curr_row[::-1])[::-1]
+            elif direction == 'u':
+                curr_row = self._grid.array[:, i]
+                self._grid.array[:, i] = _merge_array(curr_row)
 
     def move(self, direction):
         self._grid.move(direction)
+        self._merge_relevant(direction)
+        self._grid.move(direction)
+
 
 if __name__ == "__main__":
     game = Game()
     game.fill_initial()
-    game._grid.fill_debug()
+    # game._grid.fill_debug()
     print(game)
     game.move('u')
     print(game)
@@ -83,3 +126,11 @@ if __name__ == "__main__":
     print(game)
     game.move('r')
     print(game)
+
+    while True:
+        direction = input("Next move:")
+        if direction not in ['l', 'r', 'u', 'd']:
+            continue
+        game.move(direction)
+        game.fill_random_cell()
+        print(game)
