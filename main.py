8b935301b437fa03320486c1598df8cc1a0db533
Benjamin <Bendotlami@gmail.com>
2021-12-24 12:51:49 +0200

    Refactor main class

--- main.py
+++ main.py
@@ -2,87 +2,56 @@ import copy
 import numpy as np
 
 
-class Grid:
+class GameMechanic:
+    _initial_number_of_cells = 2
+    _spaces_between_cells = 1
+
     def __init__(self, grid_size=None) -> None:
         if not grid_size:
             grid_size = 4
         self.grid_size = grid_size
-        self.array = np.zeros([self.grid_size, self.grid_size], dtype=int)
-
-    def __str__(self):
-        rtn_str = ""
-        max_len = len(str(np.max(self.array))) + 1
-        for row in self.array:
-            for cell in row:
-                cell_len = len(str(cell))
-                rtn_str += f'{cell if cell != 0 else "_"}{" "*(max_len - cell_len)}'
-            rtn_str += '\n'
-        return rtn_str
-
-    def __eq__(self, other):
-        if not isinstance(other, Grid):
-            return False
-        return self.grid_size == other.grid_size and np.array_equal(self.array, other.array)
-
-    def fill_debug(self):
-        self.array = np.arange(0, self.grid_size ** 2).reshape([self.grid_size, self.grid_size])
-
-    def get_empty_spot(self) -> np.ndarray:
-        return np.argwhere(self.array == 0)
-
-    def apply_function_by_direction(self, direction, func):
-        for i in range(self.grid_size):
-            if direction == 'r':
-                curr_row = self.array[i, :]
-                self.array[i, :] = func(curr_row[::-1])[::-1]
-            elif direction == 'l':
-                curr_row = self.array[i, :]
-                self.array[i, :] = func(curr_row)
-            elif direction == 'd':
-                curr_row = self.array[:, i]
-                self.array[:, i] = func(curr_row[::-1])[::-1]
-            elif direction == 'u':
-                curr_row = self.array[:, i]
-                self.array[:, i] = func(curr_row)
-
-    def move(self, direction):
-        def move_to_end(array):
-            non_zero = array[np.where(array != 0)]
-            padding = np.zeros([self.grid_size - len(non_zero)], dtype=int)
-            return np.concatenate([non_zero, padding])
-
-        self.apply_function_by_direction(direction, move_to_end)
-
-    def get_cell_value(self, x, y):
-        return self.array[x, y]
-
-
-class Game:
-    _initial_number_of_cells = 2
-
-    def __init__(self) -> None:
-        self._grid = Grid(grid_size=4)
+        self.grid = np.zeros([self.grid_size, self.grid_size], dtype=int)
         self._score = 0
         self.fill_initial()
 
+    def __eq__(self, other):
+        if not isinstance(other, GameMechanic):
+            return False
+        return self.grid_size == other.grid_size and np.array_equal(self.grid,
+                                                                    other.grid) and self.get_score() == other.get_score()
+
+    def stringify_board(self):
+        rtn_str = ""
+        max_len = len(str(np.max(self.grid))) + self._spaces_between_cells
+        for row in self.grid:
+            for cell in row:
+                cell_len = len(str(cell))
+                rtn_str += f'{cell if cell != 0 else "_"}{" " * (max_len - cell_len)}'
+            rtn_str += '\n' * self._spaces_between_cells
+        return rtn_str
+
     def __str__(self):
-        return "\n".join([str(self.get_score()), self._grid.__str__()])
+        rtn_str = ""
+        # score
+        rtn_str += f"{str(self.get_score())}\n"
+        # board
+        rtn_str += self.stringify_board()
+        return rtn_str
 
-    def get_score(self):
-        return self._score
-
+    # Score handling
     def _add_score(self, merge_value):
         self._score += merge_value
 
-    def get_empty_spots(self) -> np.ndarray:
-        return self._grid.get_empty_spot()
+    # Board filling
+    def _fill_debug(self):
+        self.grid = np.arange(0, self.grid_size ** 2).reshape([self.grid_size, self.grid_size])
 
     def _fill_random_cells(self, size):
         spots = self.get_empty_spots()
         np.random.shuffle(spots)
         empty_spots = spots[0: size]
         random_values = np.random.choice([2, 4], p=[0.75, 0.25], size=size)
-        self._grid.array[empty_spots[:, 0], empty_spots[:, 1]] = random_values
+        self.grid[empty_spots[:, 0], empty_spots[:, 1]] = random_values
 
     def fill_initial(self):
         self._fill_random_cells(self._initial_number_of_cells)
@@ -90,12 +59,40 @@ class Game:
     def fill_random_cell(self):
         self._fill_random_cells(1)
 
+    def get_empty_spots(self) -> np.ndarray:
+        return np.argwhere(self.grid == 0)
+
+    # Move handling
+    def apply_function_by_direction(self, direction, func):
+        for i in range(self.grid_size):
+            if direction == 'r':
+                curr_row = self.grid[i, :]
+                self.grid[i, :] = func(curr_row[::-1])[::-1]
+            elif direction == 'l':
+                curr_row = self.grid[i, :]
+                self.grid[i, :] = func(curr_row)
+            elif direction == 'd':
+                curr_row = self.grid[:, i]
+                self.grid[:, i] = func(curr_row[::-1])[::-1]
+            elif direction == 'u':
+                curr_row = self.grid[:, i]
+                self.grid[:, i] = func(curr_row)
+
+    def push_values_to_edge(self, direction):
+        def push_to_end(array):
+            non_zero = array[np.where(array != 0)]
+            padding = np.zeros([self.grid_size - len(non_zero)], dtype=int)
+            return np.concatenate([non_zero, padding])
+
+        self.apply_function_by_direction(direction, push_to_end)
+
     def _merge_relevant(self, direction):
         """
         To be called after move!
         :param direction: Move direction
         :return: None
         """
+
         def _merge_array(array):
             """
             default is right move
@@ -110,19 +107,29 @@ class Game:
                 i += 1
             return array
 
-        self._grid.apply_function_by_direction(direction, _merge_array)
+        self.apply_function_by_direction(direction, _merge_array)
 
     def move(self, direction):
-        grid_before = copy.deepcopy(self._grid)
-        self._grid.move(direction)
+        grid_before = copy.deepcopy(self.grid)
+        self.push_values_to_edge(direction)
         self._merge_relevant(direction)
-        self._grid.move(direction)
-        if grid_before != self._grid:
+        self.push_values_to_edge(direction)
+        if not np.array_equal(grid_before, self.grid):
             self.fill_random_cell()
+
+    # API
+    def get_cell_value(self, x, y):
+        return self.grid[x, y]
+
+    def get_score(self):
+        return self._score
+
+    def get_grid_size(self):
+        return self.grid_size
 
 
 if __name__ == "__main__":
-    game = Game()
+    game = GameMechanic()
     # game._grid.fill_debug()
     print(game)
 
