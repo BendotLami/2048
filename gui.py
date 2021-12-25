fafba579c91cdefa74711abb8d8664b458d911c0
Ben Lami <bendotcom911@gmai.com>
2021-12-25 09:49:07 +0200

    Simple color mechanics

--- gui.py
+++ gui.py
@@ -1,6 +1,11 @@
 from main import GameMechanic
 import tkinter as tk
 from tkinter import ttk
+from math import log2
+
+
+def render_rgb(rgb):
+    return "#%02x%02x%02x" % rgb
 
 
 class GameGui:
@@ -8,6 +13,9 @@ class GameGui:
     _right_keys = ["<l>", "<d>", "<Right>"]
     _up_keys = ["<w>", "<k>", "<Up>"]
     _down_keys = ["<s>", "<j>", "<Down>"]
+
+    _gray = 160, 160, 160
+    _max_color_gradient = 5
 
     def init_window(self):
         window = tk.Tk()
@@ -36,12 +44,26 @@ class GameGui:
         self._bind_keys()
         self._render_display()
 
+    @staticmethod
+    def _render_cell_text(value):
+        return value if value else ''
+
+    @staticmethod
+    def _render_cell_color(value):
+        if value == 0:
+            return render_rgb(GameGui._gray)
+        log_value = int(log2(value)) - 1  # minus one for values [0,..]
+        relative_value = ((GameGui._max_color_gradient - log_value) / GameGui._max_color_gradient)
+        blue_value = int(204 * relative_value)
+        blue_value = min(255, blue_value)
+        return render_rgb((255, 255, blue_value))
+
     def _render_display(self):
         for i in range(self.grid_size):
             for j in range(self.grid_size):
                 cell_value = self.game.get_cell_value(i, j)
-                self._labels[i][j]["text"] = f"{cell_value if cell_value else ''}"
-                self._labels[i][j]["bg"] = "gray" if cell_value == 0 else "yellow"
+                self._labels[i][j]["text"] = self._render_cell_text(cell_value)
+                self._labels[i][j]["bg"] = self._render_cell_color(cell_value)
 
     def _make_a_move(self, direction):
         print(f"Making a move! {direction}")
@@ -69,6 +91,6 @@ class GameGui:
 
 
 if __name__ == "__main__":
-    ggui = GameGui(2)
+    ggui = GameGui()
     ggui.mainloop()
     exit(0)
