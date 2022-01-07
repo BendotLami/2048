00bc18b1343b5957230135772f6c59ae0eaee910
Benjamin <Bendotlami@gmail.com>
2022-01-07 17:25:12 +0200

    Started main structure

---
+++ train.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+class NN_2048(nn.Module):
+    def __init__(self):
+        super(NN_2048, self).__init__()
+        # ...
+
+    def forward(self, x):
+        return x
